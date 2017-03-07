152b4383afe77655c32b8189837490d9597b7fde
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-07 18:19:48 -0500

    Fixes conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,12 +3,17 @@ name1 = gets.chomp
 puts "How old are you? What year were you born?"
 age = gets.chomp 
 year =gets.chomp
+
+age_check = (age.to_i == 2017-year.to_i) 
+
+puts age_check
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 gb = gets.chomp
 if gb == 'y'
 	gb = true
 elsif gb =="n"
 	gb = false
+end
 puts "Would you like to enroll in the company’s health insurance? "
 health =gets.chomp health
 if health == 'y'
@@ -17,12 +22,12 @@ elsif health =="n"
 	health = false
 end
 
-age_check = (age==2017-year)
+
 
 case age_check
 when age_check && (gb || health)
 	result="Probably not a vampire."
-when !age_check && !(gb||health)
+when !age_check && (!gb || !health)
 	result="Probably a vampire."
 when !age_check && !gb && !health
 	result="Almost certainly a vampire."
