1a6fb3c5bb63a529e145cabdf8885fe71db74ce8
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-07 19:11:38 -0500

    Release 2

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,17 +1,47 @@
-puts "What is the hamster's name?"
-name = gets.chomp
-puts "What is your preferred volume level?(1-10)"
-volume = gets.chomp
+
+name = nil
+loop do
+	puts "What is the hamster's name?"
+	input = gets.chomp
+	if input != ""
+		name = input
+		break
+	end
+end
+
+volume = nil
+loop do 
+	puts "What is your preferred volume level?(1-10)"
+	input = gets.chomp
+	if input.to_i >=1 && input.to_i<=10
+		volume = input.to_i
+		break
+	end
+end
+
 puts "What fur color?"
 color = gets.chomp
-puts "Is the hamster a good candidate for adoption?"
-adoption = gets.chomp
-puts "Approximately how old is the hamser?"
+
+adoption=nil
+loop do
+	puts "Is the hamster a good candidate for adoption?"
+	input = gets.chomp
+	if input == 'yes' || input == 'no'
+		adoption = input
+		break
+	end
+end
+
+puts "Approximately how old is the hamster?"
 age = gets.chomp
 
 if age == ""
 	age = nil
 end
+
+
+age = age.to_i
+
 
 puts name
 puts volume
