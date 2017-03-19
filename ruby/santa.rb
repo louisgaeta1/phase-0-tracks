c31098faa0c76bb588b98d52abd5e3e0516c42e1
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-19 14:50:22 -0400

    Adds getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,11 +14,58 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+
+	def celebrate_birthday
+		@age+=1
+	end
+
+	def get_mad_at (reindeer)
+		@reindeer_ranking.push(@reindeer_ranking.slice(@reindeer_ranking.index(reindeer)))
+		puts "Santa is mad at #{@reindeer_ranking.last}"
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def gender
+		@gender
+	end
 end
 
+=begin
 claus = Santa.new
 
-#claus.initialize
+claus.initialize
 claus.speak
 claus.eat_milk_and_cookies("oreo")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+=end
+
+santa = Santa.new("male", "white")
+puts "Santa is currently a #{santa.age} year old #{santa.ethnicity} #{santa.gender}."
+
+santa.get_mad_at("Dasher")
+
+56.times do 
+	santa.celebrate_birthday
+end
+
+santa.gender = "transgender"
+
+puts "Santa is currently a #{santa.age} year old #{santa.ethnicity} #{santa.gender}."
 
