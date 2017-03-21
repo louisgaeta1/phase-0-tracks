234ddd8b4dcbf8c6a83373d3f160e134da6805cf
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-20 20:17:22 -0400

    Psuedocode shopping cart methods

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Add items as keys to Grocery List hash
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: Hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+	#Add a new Key value pair to the the list hash
+	#If quantity is specified, attach quantity as a value for the key
+# output: updated list
+
+# Method to remove an item from the list
+# input: list, item name
+# steps:
+	#Search the hash for the item name
+	#Delete the key associated with that name
+# output: updated list
+
+# Method to update the quantity of an item
+# input: list , item name, new quantity
+# steps: 
+	#Search the hash for the item name
+	#Assign the new quantity to the key
+# output: updated list
+
+# Method to print a list and make it look pretty
+# input: list
+# steps:
+	#Print each key and value: **Key**: x**Value**
+# output: **Key**: x**Value** for each item in the list
+
