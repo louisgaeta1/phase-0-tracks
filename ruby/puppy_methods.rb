335b7e78cdda492cf9da661d2d64cb1a7dd1eb41
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-21 18:47:23 -0400

    Adds Roll over and dog years methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,9 +11,20 @@ class Puppy
   	end
   end
 
+  def roll_over 
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	human_years * 7
+  end
+
+
 end
 
 fido = Puppy.new
 
 fido.fetch("bone")
 fido.speak(5)
+fido.roll_over
+puts "Fido is #{fido.dog_years(4)} years old in dog years."
