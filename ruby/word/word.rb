75957f93e64018ce4b175d13abb2ef513c997d4d
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-22 19:07:53 -0400

    Psuedocode and testing file

---
+++ ruby/word/word.rb
@@ -0,0 +1,72 @@
+=begin 
+Game class
+atrributes:
+	number of guesses
+	answer
+	guessed letters
+	blanks 
+	solved 
+methods
+	initialize
+	display
+	check letter
+	check solved
+
+initalize method
+input: word
+steps: 
+	number of guesses = 0 
+	answer = word.split('')
+	guessed letters =[]
+	solved = false
+	blanks = []
+	for each item in answer array, push a "_" onto the blanks array
+output:
+	nil
+
+
+check letter method
+input: letter
+steps:
+	push letter onto guessed letters array
+	iterate through answer array and check for letter
+	if letter is found, update blanks array at the same index with the guessed letter and return true
+	if its not found return false
+output:
+	true or false
+
+display method
+input: none
+steps:
+	print blanks.join(" ")
+output:
+	ex. _ _ _ _ _ t 
+
+check solved method
+input: none
+steps:
+	iterate through blanks array and check for a "_"
+	if "_" is found return false
+	if not return true
+output: true or false
+
+driver
+Input a word to guess
+Call an instance of game with the word as a parameter
+loop until the number of guesses = the length of the word + 3
+run display method
+Ask for input of a letter
+check that letter
+check if solved
+	if solved break
+increment number of guesses 
+end loop
+
+if solved is true
+	display finished word and victory message
+if solved is false
+	display finished word and losing message
+
+=end
+
+
---
+++ ruby/word/word_rspec.rb
@@ -0,0 +1,31 @@
+require_relative 'word'
+
+describe Game do
+	let(:game) {Game.new("cash")}
+
+	it "initalizes the game" do
+		expect (game.answer).to eq ["c","a","s","h"]
+	end
+
+	it "populates answer display" do
+		expect(game.blanks.length).to eq game.answer.length
+	end
+
+	it "checks user input for matching letter in answer " do 
+		expect(game.check_letter("a")).to eq true
+		expect(game.blanks).to eq ["_","a","_","_"]
+		expect(game.check_letter("r")).to eq false
+	end
+
+	it "prints the current guessed letters with blanks" do
+		expect(game.display).to output('_ _ _ _').to_stdout
+	end
+
+	it "checks to see if the answer was solved" do
+		game.check_letter("c")
+		game.check_letter("a")
+		game.check_letter("s")
+		game.check_letter("h")
+		expect(game.check_solved).to eq true
+	end
+end
