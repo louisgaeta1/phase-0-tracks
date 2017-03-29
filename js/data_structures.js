861e4f77b4cfed1136c2f2191d040f6811e647b6
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-29 15:31:24 -0400

    Adds addProperties function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,21 @@
-
-
+var addProperties = function(names, colors){
+	object = {};
+	for (var i = 0; i < names.length; i++){
+		object[names[i]] = colors[i];
+	}
+	return object;
+};
 
 var colors = ["Blue", "Red", "Green", "Black"];
 var names = ["Ed", "Jimbo", "Lou", "Rich"];
 
+
 colors.push("Yellow");
 names.push("George");
 
-console.log(colors);
-console.log(names);
+var result = addProperties(names,colors);
+
+console.log(result);
+
+
+
