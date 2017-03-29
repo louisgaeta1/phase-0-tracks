6fef36bf4dc33cd0cc43c8acf71748b123ce39ea
Lou Gaeta <louis.gaeta1@gmail.com>
2017-03-29 15:15:54 -0400

    Adds color and names array

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+
+
+
+var colors = ["Blue", "Red", "Green", "Black"];
+var names = ["Ed", "Jimbo", "Lou", "Rich"];
+
+colors.push("Yellow");
+names.push("George");
+
+console.log(colors);
+console.log(names);
