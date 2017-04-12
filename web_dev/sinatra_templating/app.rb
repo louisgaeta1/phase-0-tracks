82bdaa45c744037f906503d5a4ae330871f64a23
Lou Gaeta <louis.gaeta1@gmail.com>
2017-04-12 16:18:43 -0400

    Copies code from Pairing

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
-require 'sinatra'
+require "sinatra"
+#require "sinatra/reloader" if development?
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -24,4 +25,19 @@ post '/students' do
   redirect '/'
 end
 
+post '/happy_birthday' do
+	@students_new_age =  db.execute("SELECT * FROM students")
+	age_incr=params['incr'].to_i
+	student_id=1
+	@students_new_age.each do |student|
+		new_age = student['age']+=age_incr
+		db.execute("UPDATE students SET age=? WHERE id=?", new_age, student_id)
+		student_id+=1
+	end
+	redirect '/'
+end
+
+get '/happy_birthday/incr' do
+	erb :index
+end
 # add static resources
