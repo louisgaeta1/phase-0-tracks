53315cbf3ab8b069dec9987c805eba4d5d20dec0
Lou Gaeta <louis.gaeta1@gmail.com>
2017-04-13 15:03:18 -0400

    Jquery Practice

--- web_dev/jquery/jquerypractice.js
+++ web_dev/jquery/jquerypractice.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
  
-    alert("Welcome");
+    //alert("Welcome");
+
+    $('div div').click(function(){
+    	if (this.style.opacity < 1){
+    		$(this).fadeTo('slow',1);
+    	}
+    	else {
+    		$(this).fadeTo('slow',0.01);
+    	}
+    })
+
+    $("button").click(function(){
+    	console.log($(this).siblings('div'));
+    	$(this).siblings('div').animate({
+    		height: 'toggle'
+    	});
+    });
  
 });
